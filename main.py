51936057fd0e6a11b29e57765943874e6fdac488
picpoint <picpoint@yandex.ru>
2024-06-22 12:15:00 +0300

    work with ipcalc

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ print(ip)
 ip_list = ip.split(".")
 print(ip_list)
 
+ip_list_bin = []
+
+for oct in ip_list:
+    oct = int(oct)
+    ip_list_bin.append(bin(oct)[2:])
+
+print(ip_list_bin)
+
+for bin_oct in ip_list_bin:
+    if len(bin_oct) < 8:
+        print(len(bin_oct))
