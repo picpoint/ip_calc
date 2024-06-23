c161f1fd1b818b111ca243f904a0cafe51bf22c8
picpoint <picpoint@yandex.ru>
2024-06-23 21:18:38 +0300

    make convert ip and mask to binary

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 print("Программа показывающая подсеть по маске")
 print("---------------------------------------")
 ip = input("Введите IP адресс: ")
-# mask = input("Введите маску: ")
+mask = input("Введите маску: ")
 
-print(ip)
-# print(mask)
+def ip_to_binary(ip):
+    ip_list_bin = []
+    res_lst = []
 
-ip_list = ip.split(".")
-print(ip_list)
+    print(ip)
+    ip_list = ip.split(".")
 
-ip_list_bin = []
+    for oct in ip_list:
+        oct = int(oct)
+        ip_list_bin.append(bin(oct)[2:])
 
-for oct in ip_list:
-    oct = int(oct)
-    ip_list_bin.append(bin(oct)[2:])
+    for i in ip_list_bin:
+        if len(i) < 8:
+            lngth = 8 - len(i)
+            while lngth != 0:
+                i = "0" + i
+                lngth -=1
+            res_lst.append(i)
+        else:
+            res_lst.append(i)
 
-print(ip_list_bin)
+    print(res_lst)
 
-for bin_oct in ip_list_bin:
-    if len(bin_oct) < 8:
-        print(len(bin_oct))
+
+def mask_to_binary(mask):
+    print(mask)
+    mask_list = mask.split(".")
+    mask_list_binary = []
+    res_mask_list_binary = []
+
+    for i in mask_list:
+        oct = bin(int(i))
+        mask_list_binary.append(oct[2:])
+
+    for oct in mask_list_binary:
+        while len(oct) < 8:
+            oct = "0" + oct
+        res_mask_list_binary.append(oct)
+    print(res_mask_list_binary)
+
+print()
+ip_to_binary(ip)
+mask_to_binary(mask)
