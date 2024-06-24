a6e4c138f905a5bf76248ac58798c42ed82216c8
picpoint <picpoint@yandex.ru>
2024-06-24 08:51:30 +0300

    wirk with split ip by mask

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ print("---------------------------------------")
 ip = input("Введите IP адресс: ")
 mask = input("Введите маску: ")
 
+'''
+Функция перевода IP в двоичный формат
+'''
 def ip_to_binary(ip):
     ip_list_bin = []
     res_lst = []
-
-    # print(ip)
     ip_list = ip.split(".")
 
     for oct in ip_list:
@@ -27,8 +28,10 @@ def ip_to_binary(ip):
     return res_lst
 
 
+'''
+Функция перевода маски в двоичный формат
+'''
 def mask_to_binary(mask):
-    # print(mask)
     mask_list = mask.split(".")
     mask_list_binary = []
     res_mask_list_binary = []
@@ -48,10 +51,32 @@ ip_list = ip_to_binary(ip)
 mask_list = mask_to_binary(mask)
 
 
+
 def apply_mask_to_ip(ip, mask):
     print(ip)
     print(mask)
+    counter_bit = 0
+    ip_bin_str = ""
+    network_ip = ""
+    host_ip = ""
+
+    for oct_mask in mask:
+        for bit_mask in oct_mask:
+            # print(bit_mask)
+            if bit_mask == "1":
+                counter_bit += 1
+    print(counter_bit)
+
+    for oct_ip in ip:
+        for bit_ip in oct_ip:
+            ip_bin_str += bit_ip
+
+    print(ip_bin_str)
+
+     
+
+
+
+
 
 apply_mask_to_ip(ip_list, mask_list)
-
-print()
