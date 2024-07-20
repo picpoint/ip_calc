52558b6e196eabf84744d08ee8a0a7e7b08ba1e3
picpoint <picpoint@yandex.ru>
2024-07-20 21:04:55 +0300

    made calculate broadcast adress

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def apply_mask_to_ip(ip, mask):
     for i in range(0, 32 - len(network_adress_part)):
         full_network_adress_bin += "0"
 
-    print(f"Full NA bin - {full_network_adress_bin}")
+    # print(f"Full NA bin - {full_network_adress_bin}")
     full_network_adress_bin_list = []
     counter = 0
     tmp = ""
@@ -154,16 +154,37 @@ def apply_mask_to_ip(ip, mask):
     вычисление broadcast адреса
     '''
     network_adress_part_bin = full_network_adress_bin[:counter_bit_mask]
-    print(f"Часть адреса сети - {network_adress_part_bin}")
+    # print(f"Часть адреса сети - {network_adress_part_bin}")
 
     for i in range(0, 32 - counter_bit_mask):
         network_adress_part_bin += "1"
 
     broadcast_full_adress_bin = network_adress_part_bin
-    print(broadcast_full_adress_bin)
+    # print(broadcast_full_adress_bin)
+
+    counter_broadcast_octet = 0
+    broadcast_full_adress_list = []
+    tmp = ""
+
+    for i in broadcast_full_adress_bin:
+        counter_broadcast_octet += 1
+        if counter_broadcast_octet <= 8:
+            tmp += i
+            if counter_broadcast_octet == 8:
+                broadcast_full_adress_list.append(tmp)
+                tmp = ""
+                counter_broadcast_octet = 0
+
+    # print(broadcast_full_adress_list)
+    broadcast_adress = ""
+
+    for i in broadcast_full_adress_list:
+        broadcast_adress = broadcast_adress + str(int(i, 2)) + "."
+    broadcast_adress = broadcast_adress[:-1]
+    print(f"Широковешятельный адрес - {broadcast_adress}")
 
 
-
+    
 
 apply_mask_to_ip(ip_list, mask_list)
 
