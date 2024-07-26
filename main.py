e7ae3502412cd07bee323226b03fe8e3688798d9
picpoint <picpoint@yandex.ru>
2024-07-26 08:29:08 +0300

    made refactoring code

--- main.py
+++ main.py
@@ -90,40 +90,10 @@ def apply_mask_to_ip(ip, mask):
     hosts_in_network = 2 ** (len(host_adress_binary_part)) - 2
     print(f"Количество хостов в сети - {hosts_in_network}")
 
-    # return full_network_adress_bin
-
-    calculate_broadcast_address(full_network_adress_bin, counter_bit_mask)
+    '''
+    Вызов модуля вычисления широковещательного адреса
+    '''
     broadcast_full_adress_bin = calculate_broadcast_address(full_network_adress_bin, counter_bit_mask)
-    # '''
-    # вычисление broadcast адреса
-    # '''
-    # network_adress_part_bin = full_network_adress_bin[:counter_bit_mask]
-    #
-    # for i in range(0, 32 - counter_bit_mask):
-    #     network_adress_part_bin += "1"
-    #
-    # broadcast_full_adress_bin = network_adress_part_bin
-    #
-    # counter_broadcast_octet = 0
-    # broadcast_full_adress_list = []
-    # tmp = ""
-    #
-    # for i in broadcast_full_adress_bin:
-    #     counter_broadcast_octet += 1
-    #     if counter_broadcast_octet <= 8:
-    #         tmp += i
-    #         if counter_broadcast_octet == 8:
-    #             broadcast_full_adress_list.append(tmp)
-    #             tmp = ""
-    #             counter_broadcast_octet = 0
-    #
-    # broadcast_adress = ""
-    #
-    # for i in broadcast_full_adress_list:
-    #     broadcast_adress = broadcast_adress + str(int(i, 2)) + "."
-    # broadcast_adress = broadcast_adress[:-1]
-    # print(f"Широковещятельный адрес - {broadcast_adress}")
-
 
     '''
     Вызов модуля вычисления начального адреса хоста
